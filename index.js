65ad90e6725c3a803c41a45d34b60d24517604dd
lobo <lobomfz@protonmail.com>
2022-08-16 20:50:59 -0300

    Revert "who needs toggledKeys anyway"

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import robot from "robotjs";
 import { exec } from "child_process";
 
 var modList, clientId, clientSecret, channelName, time;
+var toggledKeys = [];
 
 var isActive = 1;
 const tokenData = JSON.parse(await promises.readFile("./tokens.json", "UTF-8"));
@@ -71,6 +72,16 @@ function updateMods() {
 	});
 }
 
+function releaseToggledKey(keyName) {
+	// If for some reason the key is already in toggledKeys (held down), let go and remove.
+	if (keys.includes(keyName)) {
+		if (toggledKeys.includes(keyName)) {
+			robot.keyToggle(keyName, "up");
+			toggledKeys.splice(toggledKeys.indexOf(keyName), 1);
+		}
+	}
+}
+
 async function move(dir1, dir2, time = 600) {
 	if (dir1) robot.keyToggle(directions[dir1], "down");
 	if (dir2) robot.keyToggle(directions[dir2], "down");
@@ -83,6 +94,7 @@ async function move(dir1, dir2, time = 600) {
 async function roll(dir1, dir2, time = 2000) {
 	if (dir1) robot.keyToggle(directions[dir1], "down");
 	if (dir2) robot.keyToggle(directions[dir2], "down");
+	releaseToggledKey("r");
 	robot.keyToggle("r", "down");
 	await sleep(time);
 	if (dir1) robot.keyToggle(directions[dir1], "up");
@@ -116,6 +128,7 @@ async function look(dir, time = 600) {
 }
 
 async function jump(dir1, dir2, long, time = 900) {
+	releaseToggledKey("a");
 	robot.keyTap("a");
 	if (dir1) robot.keyToggle(directions[dir1], "down");
 	if (dir2) robot.keyToggle(directions[dir2], "down");
@@ -128,6 +141,7 @@ async function jump(dir1, dir2, long, time = 900) {
 
 async function press(key) {
 	if (key) {
+		releaseToggledKey(key);
 		robot.keyTap(key);
 	}
 	return 1;
@@ -135,6 +149,7 @@ async function press(key) {
 
 async function hold(key, time = 2000) {
 	if (key in directions) key = directions[key];
+	releaseToggledKey(key);
 	robot.keyToggle(key, "down");
 	await sleep(time);
 	robot.keyToggle(key, "up");
@@ -422,8 +437,14 @@ chatClient.onMessage(async (channel, user, message, msg) => {
 	if (isActive == 1) {
 		console.log(message);
 
-		if (mSplit[0] == "keepdown" && keys.includes(mSplit[1])) {
-			robot.keyToggle(mSplit[1], "down");
+		if (mSplit[0] == "toggle" && keys.includes(mSplit[1])) {
+			if (toggledKeys.includes(mSplit[1])) {
+				robot.keyToggle(mSplit[1], "up");
+				toggledKeys.splice(toggledKeys.indexOf(mSplit[1]), 1);
+			} else {
+				robot.keyToggle(mSplit[1], "down");
+				toggledKeys.push(mSplit[1]);
+			}
 			return 0;
 		}
 
